eb75d53f7ad7b61bae45b0dd3d75289e601b14fb
gonzalo1221 <gonzaloaraujo001@gmail.com>
2024-05-20 21:37:41 -0500

    update

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
 let user = {
-    "id": "1",
+    "id": 1,
     "nombre": "jose",
     "apellido": "benavidez",
-    "Telefono": "123",
-    "Producto": "leche"
+    "telefono": 3045678992,
+    "producto": 3
+};
+let productos = {
 };
 
 let nextId = 1; // Contador para el próximo ID
+
+function agregarProducto(id, nombre) {
+  productos[id.toString()] = {"nombre": nombre};
+}
+
+// Uso de la función para agregar un nuevo producto
+agregarProducto(1,"Leche")
+agregarProducto(2, "Arroz");
+agregarProducto(3, "Pan");
 
 function guardar() {
     const name = document.getElementById("nombre").value;
@@ -22,19 +33,35 @@ function guardar() {
         id: ids,
         nombre: name,
         apellido: lastname,
-        Telefono: phone,
-        Producto: product
+        telefono: phone,
+        producto: product
     };
   
     console.log(user);
     llenartabla()
-
-    document.getElementById("nombre").value = "";
-    document.getElementById("apellidos").value = "";
-    document.getElementById("telefono").value = "";
-    document.getElementById("producto").selectedIndex = 0;
+    limpiarCampos()
 }
 document.addEventListener('DOMContentLoaded', llenartabla);
+document.addEventListener('DOMContentLoaded', mostrarProductos);
+
+function mostrarProductos() {
+  console.log(productos)
+  const selectElement = document.querySelector('.form-select');
+  selectElement.innerHTML = '<option selected>Selecciona un Producto</option>';
+  
+  for (const id in productos) {
+    if (productos.hasOwnProperty(id)) {
+      const producto = productos[id];
+      const option = document.createElement('option');
+      option.value = id;
+      option.textContent = producto.nombre;
+      selectElement.appendChild(option);
+    }
+  }
+}
+
+
+
 
 function llenartabla() {
     const tabla = document.querySelector('.table');
@@ -46,12 +73,93 @@ function llenartabla() {
         <th scope="row">${usuario.id}</th>
         <td>${usuario.nombre}</td>
         <td>${usuario.apellido}</td>
-        <td>${usuario.Telefono}</td>
-        <td>${usuario.Producto}</td>
+        <td>${usuario.telefono}</td>
+        <td>${usuario.producto}</td>
         <td>
             <button class="btn btn-primary btn-sm" onclick="cargarUsuario(${usuario.id})">Actualizar</button>
             <button class="btn btn-danger btn-sm" onclick="eliminarUsuario(${usuario.id})">Eliminar</button>
         </td>
     `;
     tabla.appendChild(tr);
+}
+function cargarUsuario(id) {
+
+      const usuarios = user;
+      console.log(usuarios);
+
+        document.getElementById('nombre').value = usuarios.nombre;
+        document.getElementById('apellidos').value = usuarios.apellido;
+        document.getElementById('telefono').value = usuarios.telefono;
+        document.getElementById('producto').value = usuarios.producto;
+        mostrarCancelar();
+
+      // Cambiar el texto y la funcionalidad del botón del formulario a "Actualizar"
+      const botonGuardar = document.querySelector('button[type="guardar"]');
+      botonGuardar.textContent = 'Actualizar';
+      botonGuardar.onclick = function() {
+        actualizarUsuario(id);
+        ocultarCancelar();
+      };
+
+      // Agregar botón "Cancelar" y cambiar su funcionalidad
+      const botonCancelar = document.getElementById('cancelarBtn');
+      botonCancelar.onclick = function() {
+        botonGuardar.textContent = 'Guardar';
+        botonGuardar.onclick = function() {
+          guardar();
+        };
+        botonCancelar.style.display = 'none';
+        limpiarcampos();
+}
+}
+
+function actualizarUsuario(id) {
+  const nombre = document.getElementById('nombre').value;
+  const apellidos = document.getElementById('apellidos').value;
+  const telefono = document.getElementById('telefono').value;
+  const producto = document.getElementById('producto').value;
+
+  const datosActualizar = {
+    nombre: nombre,
+    apellidos: apellidos,
+    telefono: telefono,
+    productos: producto
+  };
+
+  axios.put(`/api/actualizarusuario/${id}`, datosActualizar)
+    .then(response => {
+      alert('Usuario actualizado correctamente');
+      location.reload();
+    })
+    .catch(error => {
+      console.log('Error al actualizar el usuario: ', error);
+    });
+}
+function eliminarUsuario(id) {
+  axios.delete('/api/eliminarusuario/' + id)
+   .then(response => {
+      console.log('Usuario eliminado con éxito');
+      llenartabla();
+      location.reload();
+    })
+   .catch(error => {
+      console.log('Error al eliminar el usuario: ', error);
+    });
+}
+
+function ocultarCancelar() {
+  const botonCancelar = document.getElementById('cancelarBtn');
+  botonCancelar.style.display = 'none'; // Oculta el botón "Cancelar"
+}
+
+function mostrarCancelar() {
+  const botonCancelar = document.getElementById('cancelarBtn');
+  botonCancelar.style.display = 'inline-block'; // Muestra el botón "Cancelar"
+}
+
+function limpiarCampos() {
+  document.getElementById('nombre').value = "";
+  document.getElementById('apellidos').value = "";
+  document.getElementById('telefono').value = "";
+  document.getElementById('producto').selectedIndex = 0;
 }
